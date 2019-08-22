b910653421176481aabfa9b1b46965895073c818
James Gary <gmarchesi888@gmail.com>
2019-08-22 14:43:00 -0400

    Tutorializing.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class TodoList extends Component {
+  state = { items: ["hello", "world", "click", "me"] };
+
+  handleAdd = () => {
+    const newItems = this.state.items.concat([prompt("Enter some text")]);
+    this.setState({ items: newItems });
+    console.log(this.state);
+  };
+
+  handleRemove = i => {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  };
+
+  render() {
+    const items = this.state.items.map((item, index) => (
+      <div key={item} onClick={() => this.handleRemove(index)}>
+        <h3>{item}</h3>
+      </div>
+    ));
+
+    return (
+      <div className="container-outer">
+        <h1>I Am The App</h1>
+        <button onClick={this.handleAdd}>Add Item</button>
+        <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {items}
+        </CSSTransitionGroup>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
