3e8644219b74d5c4e9f7ecce50f48d13f559f2b2
James Gary <gmarchesi888@gmail.com>
2019-08-24 08:09:53 -0400

    Basic transition working again.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import React, { Component } from "react";
 import "./App.css";
-import { CSSTransitionGroup } from "react-transition-group";
+import { Transition } from "react-transition-group";
 
-class TodoList extends Component {
-  state = { items: ["hello", "world", "click", "me"] };
+const defaultStyle = { transition: "opacity 400ms ease-in-out", opacity: 0 };
 
-  handleAdd = () => {
-    const newItems = this.state.items.concat([prompt("Enter some text")]);
-    this.setState({ items: newItems });
-    console.log(this.state);
-  };
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 1 },
+  exited: { opacity: 0 }
+};
 
-  handleRemove = i => {
-    let newItems = this.state.items.slice();
-    newItems.splice(i, 1);
-    this.setState({ items: newItems });
+class App extends Component {
+  state = { bool: false };
+
+  toggle = () => {
+    this.setState(prevState => ({
+      bool: !prevState.bool
+    }));
   };
 
   render() {
-    const items = this.state.items.map((item, index) => (
-      <div key={item} onClick={() => this.handleRemove(index)}>
-        <h3>{item}</h3>
-      </div>
-    ));
+    const {
+      toggle,
+      state: { bool }
+    } = this;
 
     return (
-      <div className="container-outer">
-        <CSSTransitionGroup
-          transitionName="example"
-          transitionAppear={true}
-          transitionAppearTimeout={500}
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
-        >
-          
-          <button onClick={this.openContainer}>Open</button>
-          
-        </CSSTransitionGroup>
+      <div>
+        <div className="spacer10" />
+        <button onClick={toggle}>Fade</button>
+        <Transition in={bool} timeout={400}>
+          {status => (
+            <div
+              style={{
+                ...defaultStyle,
+                ...transitionStyles[status]
+              }}
+            >
+              <h2>This Text Will Fade In And Out</h2>
+              {console.log(status)}
+            </div>
+          )}
+        </Transition>
       </div>
     );
   }
 }
 
-export default TodoList;
+export default App;
